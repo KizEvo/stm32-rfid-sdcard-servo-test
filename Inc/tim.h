a0b32e69068f569fa1800ecc0c27e80359da0269
Duc Phu <vandungqc03@gmail.com>
2024-02-28 19:03:54 +0700

    Added TIM PWM mode, Servo funcs

--- Inc/tim.h
+++ Inc/tim.h
@@ -4,7 +4,10 @@
 #include <stdint.h>
 
 void TIM_PWM_InitConfig(void);
-
+void TIM_PWM_RotateServo(float angle);
+void TIM_PWM_EnableServo(void);
+void TIM_PWM_DisableServo(void);
+	
 void delayMs(uint32_t time);
 void startTimeOutMs(uint32_t time);
 void endTimeOutMs(void);
--- Src/tim.c
+++ Src/tim.c
@@ -19,6 +19,8 @@
 
 static uint32_t CLOCK_PERIOD_ONE_MS = 6999;	// N = 7000 (required) => N - 1 = 6999 (period) = 1 (ms)
 static volatile uint8_t timeOutFlag = 0;
+static uint16_t TIM_PWM_PRE = 250 - 1;
+static uint16_t TIM_PWM_ARR = 4480;
 
 /*============================================================================
  *
@@ -104,7 +106,6 @@ void TIM_PWM_InitConfig(void)
 {
 	// Turn on TIM2 clock
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
 	
 	// Config IO for TIM2_CH2
 	GPIO_InitTypeDef TIM2_CH2_IO;
@@ -114,25 +115,59 @@ void TIM_PWM_InitConfig(void)
 	
 	GPIO_Init(GPIOA, &TIM2_CH2_IO);
 	
-	uint16_t arrValue = 2240 * 2;
-	uint16_t dutyCycle = 5;
-	
 	// Setup TIM2 parameters
 	TIM_TimeBaseInitTypeDef TIM2_TimeBase;
 	TIM2_TimeBase.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM2_TimeBase.TIM_Prescaler = 250 - 1;
-	TIM2_TimeBase.TIM_Period = arrValue;
+	TIM2_TimeBase.TIM_Prescaler = TIM_PWM_PRE;
+	TIM2_TimeBase.TIM_Period = TIM_PWM_ARR;
 	
 	TIM_ARRPreloadConfig(TIM2, ENABLE);
-	
+	TIM_TimeBaseInit(TIM2, &TIM2_TimeBase);
+
+	// Setup TIM2 output compare parameters
 	TIM_OCInitTypeDef TIM2_PWM;
 	TIM2_PWM.TIM_OCMode = TIM_OCMode_PWM1;
 	TIM2_PWM.TIM_OCPolarity = TIM_OCPolarity_High;
-	TIM2_PWM.TIM_Pulse = (arrValue * dutyCycle) / 100;
 	TIM2_PWM.TIM_OutputState = TIM_OutputState_Enable;
-	
+	TIM2_PWM.TIM_Pulse = (uint16_t)((((float)TIM_PWM_ARR) * 2) / 100.0);
 	TIM_OC2Init(TIM2, &TIM2_PWM);
-	TIM_TimeBaseInit(TIM2, &TIM2_TimeBase);
+}
+
+/*============================================================================
+ *
+ * Rotate Servo according to the "angle" parameter
+ *
+ *============================================================================
+ */
+void TIM_PWM_RotateServo(float angle)
+{
+	float dutyCycle = 0.0444444 * angle + 2.0;
+
+	uint16_t pulse = (uint16_t)((((float)TIM_PWM_ARR) * dutyCycle) / 100.0);
+	
+	TIM_SetCompare2(TIM2, pulse);
+}
+
+/*============================================================================
+ *
+ * Enable Servo
+ *
+ *============================================================================
+ */
+void TIM_PWM_EnableServo(void)
+{
 	TIM_CCxCmd(TIM2, TIM_Channel_2, TIM_CCx_Enable);
 	TIM_Cmd(TIM2, ENABLE);
+}
+
+/*============================================================================
+ *
+ * Disable Servo
+ *
+ *============================================================================
+ */
+void TIM_PWM_DisableServo(void)
+{
+	TIM_CCxCmd(TIM2, TIM_Channel_2, TIM_CCx_Disable);
+	TIM_Cmd(TIM2, DISABLE);
 }
